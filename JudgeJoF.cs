45bb9908322c15bebe2e8f6bdbedbb6fabee2dea
Endlin Boeingstein <endlinboeingstein@gmail.com>
2022-09-06 19:45:40 +0800

    添加项目文件。

---
+++ JudgeJoF.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+//建立判断类
+class JudgeJoF
+{
+	//判断完成后路径定义
+	public string Path = null;
+	//判断完成后xfl路径定义
+	public string Fpath = null;
+	//判断完成后位图路径定义
+	public string Spath = null;
+	
+	//创建xr实例
+	XflReader xr = new XflReader();
+	//递归判断
+	public void ReJudge(string filepath)
+    {
+        try
+        {
+			if (File.Exists(filepath))
+			{
+				Console.WriteLine("已检测到为文件，请拖入文件夹，而不是单独文件");
+				//递归判断
+				ReJudge(Console.ReadLine().Trim('"'));
+			}
+			else if (Directory.Exists(filepath))
+			{
+				Console.WriteLine("检测到为文件夹，处理中......");
+				this.Path = filepath;
+			}
+			else
+			{
+				Console.WriteLine("未检测到文件或文件夹！请检查！");
+				//递归判断
+				ReJudge(Console.ReadLine().Trim('"'));
+			}
+		}
+		catch
+		{
+			Console.WriteLine("ERROR");
+		}
+	}
+
+	//判断是file还是dir路径
+	public void Judge(string filepath)
+	{
+
+		try
+		{
+			//判断是文件还是文件夹
+			ReJudge(filepath);
+			this.Fpath = this.Path;
+			Console.WriteLine("含xfl文件夹的总文件夹录入完成，处理中......");
+			Console.WriteLine("请将含位图文件夹拖入窗体，并按回车键（不输入直接按回车默认位图和xfl文件夹在同一总文件夹中）");
+			this.Spath = Console.ReadLine().Trim('"');
+			if (this.Spath == "" || this.Spath == "/n/n")
+            {
+				this.Spath = this.Fpath;
+            }
+            else
+            {
+				//判断是文件还是文件夹
+				ReJudge(this.Spath);
+				this.Spath = this.Path;
+			}
+			Console.WriteLine("含位图文件夹的总文件夹录入完成，处理中......");
+			//寻找总文件夹中所有Anim.xfl文件
+			xr.XflRead(this.Fpath,this.Spath);
+			
+		}
+		catch
+		{
+			Console.WriteLine("ERROR");
+		}
+
+	}
+}
---
+++ XflReader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+//建立xfl读取类
+class XflReader
+{
+    //创建sc实例
+    SpriteCopyer sc = new SpriteCopyer();
+    //创建mnr实例
+    MainNameRewriter mnr = new MainNameRewriter();
+    //生成xfl读取部分
+    public void XflRead(string Fpath,string Spath)
+    {
+        try
+        {
+            //创建路径文件夹实例
+            DirectoryInfo TheFolder = new DirectoryInfo(Fpath);
+            //数组记录各个xfl文件信息
+            FileInfo[] xfl = TheFolder.GetFiles("Anim.xfl", SearchOption.AllDirectories);
+            FileInfo[] mxfl = TheFolder.GetFiles("main.xfl", SearchOption.AllDirectories);
+            xfl = (FileInfo[])xfl.Concat(mxfl).ToArray();
+            if (xfl.Length != 0)
+            {
+                for (int i = 0; i < xfl.Length ; i++)
+                {
+                    //创建ddr实例
+                    DOMDocumentReader ddr = new DOMDocumentReader();
+                    //创建bsc实例
+                    BigSpriteCopyer bsc = new BigSpriteCopyer();
+                    //记录装载xfl的路径
+                    string dir = xfl[i].DirectoryName;
+                    //获取dir文件夹名
+                    DirectoryInfo dirname = new DirectoryInfo(dir);
+                    //新功能更新而停用//执行位图复制操作
+                    //新功能更新而停用///sc.SpriteCopy(dir);
+                    //将位图引用录入数组
+                    ddr.DOMDocumentRead(dir + "\\" + "DOMDocument.xml");
+                    //大文件夹图片复制
+                    bsc.BigSpriteCopy(dir,Spath,ddr.sarray);
+                    Console.WriteLine(dirname.Name + "的LIBRARY位图已填充");
+                    //执行Anim名称重写操作
+                    mnr.MainNameRewrite(dir);
+                    Console.WriteLine(dirname.Name + "的xfl文件已重命名");
+                }
+            }
+            else { }
+        }
+        catch
+        {
+            Console.WriteLine("XflRead ERROR");
+            //提示按任意键继续
+            Console.WriteLine("Press any key to continue...");
+            //输入任意键退出
+            Console.ReadLine();
+        }
+    }
+}
